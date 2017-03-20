4dc05d672ef022c55148226c75e1260a5c2d9c36
Marcel Weigel <marcel.weigel@hotmail.de>
2017-03-20 15:17:24 +0100

    - change texts - add icons - rename Back button

--- 03 Code/Source/Visit Bluehands/Assets/VirtualVisit/Scripts/UI/FollowingDisplayBackItem.cs
+++ 03 Code/Source/Visit Bluehands/Assets/VirtualVisit/Scripts/UI/FollowingDisplayBackItem.cs
@@ -17,6 +17,6 @@ public class FollowingDisplayBackItem : MonoBehaviour {
         m_ButtonInfoBack.transform.localScale = new Vector3(0.1f, 0.1f, 1f);
 
         Text text = transform.FindChild("Canvas Item").FindChild("Text").GetComponent<Text>();
-        text.text = "Schließen";
+        text.text = "Back";
     }
 }
